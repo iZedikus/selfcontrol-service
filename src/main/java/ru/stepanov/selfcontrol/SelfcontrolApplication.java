214bd04f54951b5ccaf4a18bfec342fc8f8ad902
iZedikus <izedikus555@mail.ru>
2026-05-21 14:30:26 +0700

    init commit

---
+++ src/main/java/ru/stepanov/selfcontrol/SelfcontrolApplication.java
@@ -0,0 +1,13 @@
+package ru.stepanov.selfcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfcontrolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepanov/selfcontrol/SelfcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.selfcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfcontrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
